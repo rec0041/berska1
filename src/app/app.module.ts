ceeccd42dc121f98bde188cdde3e1f3632b94dda
Rachel <rec0041@mix.wvu.edu>
2022-01-24 13:24:51 -0500

    icon example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TopNavBarComponent } from 'src/navigation/topnavbar.component';
+import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
 
