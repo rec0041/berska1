93b8bc1c0491a74bb652aa19470fe6a8750636c8
Rachel <rec0041@mix.wvu.edu>
2022-02-14 17:51:11 -0500

    best sellers items

--- src/app/mock_product_list.ts
+++ src/app/mock_product_list.ts
@@ -23,5 +23,40 @@ export const mock_product_list = [
         img: "assets/leatherpants.jpg",
         price: 45.90,
         description: "Croc texture leather look straight leg pants"
+    },
+    {
+        img: "assets/FPPSWB.jpg",
+        price: 59.90,
+        description: "Faux patent platform shoes with buckle"
+    },
+    {
+        img: "assets/CTCWCP.jpg",
+        price: 119,
+        description: "Cotton trench coat with contrasting plaid"
+    },
+    {
+        img: "assets/VFLC.jpg",
+        price: 79.90,
+        description: "Vinyl faux leather coat"
+    },
+    {
+        img: "assets/KMDWRDOTN.jpg",
+        price: 49.90,
+        description: "Knit mini dress with rhinestone detail on the neckline"
+    },
+    {
+        img: "assets/WLCP.jpg",
+        price: 45.90,
+        description: "Wide-leg cargo pants"
+    },
+    {
+        img: "assets/WLTCP.jpg",
+        price: 45.90,
+        description: "Wide-leg twill cargo pants"
+    },
+    {
+        img: "assets/FLPJ.jpg",
+        price: 59.90,
+        description: "Faux leather puffer jacket"
     }
 ];
