b7adf79c580b741f0513db858ef4c5308dd79bc1
Rachel <rec0041@mix.wvu.edu>
2022-02-14 14:51:38 -0500

    mock item list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { mock_product_list } from './mock_product_list';
+import { ProductItemModel } from './product-item.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bershka';
+  products: ProductItemModel [] = [];
+
+  constructor() {
+    for (var product of mock_product_list) {
+      console.log(product);
+      this.products.push(product);
+    }
+  }
 }
---
+++ src/app/mock_product_list.ts
@@ -0,0 +1,27 @@
+export const mock_product_list = [
+    {
+        img: "assets/ribbedcorset.jpg",
+        price: 22.90,
+        description: "Ribbed sleeveless corset top with straps"
+    },
+    {
+        img: "assets/DTSD.jpg",
+        price: 49.90,
+        description: "Draped tulle strappy dress"
+    },
+    {
+        img: "assets/SWC.jpg",
+        price: 39.90,
+        description: "Sweater with chain detail"
+    },
+    {
+        img: "assets/loafers.jpg",
+        price: 59.90,
+        description: "Loafers with metallic detail"
+    },
+    {
+        img: "assets/leatherpants.jpg",
+        price: 45.90,
+        description: "Croc texture leather look straight leg pants"
+    }
+];
