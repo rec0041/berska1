f6bdca134a7ca178279c85a4cc84e01b984501be
Rachel <rec0041@mix.wvu.edu>
2022-02-15 15:31:03 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { mock_product_list } from './mock_product_list';
-import { ProductItemModel } from './product-item.model';
+// import { mock_product_list } from './mock_product_list';
+// import { ProductItemModel } from './product-item.model';
+import { ProductItem2Model } from './storefront/product-item2.model';
+import { mock_product_list2 } from './storefront/shoes/mock_product_list2';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,12 @@ import { ProductItemModel } from './product-item.model';
 })
 export class AppComponent {
   title = 'bershka';
-  
+  products: ProductItem2Model [] = [];
+
+  constructor() {
+    for (var product of mock_product_list2) {
+      console.log(product);
+      this.products.push(product);
+    }
+  }
 }
---
+++ src/app/storefront/shoes/mock_product_list2.ts
@@ -0,0 +1,37 @@
+export const mock_product_list2 = [
+    {
+        img: "assets/HHSWVS.jpg",
+        price: 35.90,
+        description: "High-heel shoes with V-shape"
+    },
+    {
+        img: "assets/SHHS.jpg",
+        price: 39.90,
+        description: "Shiny high-heel shoes"
+    },
+    {
+        img: "assets/HSWPT.jpg",
+        price: 39.90,
+        description: "Heeled shoes with a pointed toe"
+    },
+    {
+        img: "assets/SHSS.jpg",
+        price: 45.90,
+        description: "Shiny heeled slingback shoes"
+    },
+    {
+        img: "assets/VHSS.jpg",
+        price: 49.90,
+        description: "Vinyl heeled slingback shoes"
+    },
+    {
+        img: "assets/XLTSFB.jpg",
+        price: 89.90,
+        description: "XL track sole flat boots"
+    },
+    {
+        img: "assets/FQB.jpg",
+        price: 89.90,
+        description: "Flat quilted boots."
+    }
+];
