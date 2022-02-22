fa09ab26f5593518ffa6a3c2c6a4d82574fe749d
Rachel <rec0041@mix.wvu.edu>
2022-02-22 16:41:22 -0500

    top nav bars

--- src/app/storefront/accessories/accessoriespage.component.ts
+++ src/app/storefront/accessories/accessoriespage.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { mock_product_list5 } from "./mock_product_list5";
+import { mock_product_list5_2 } from "./mock_product_list5-2";
+import { ProductItem5ModelPt2 } from "./product-item5-2.model";
 import { ProductItem5Model } from "./product-item5.model";
 
 
@@ -11,11 +13,16 @@ import { ProductItem5Model } from "./product-item5.model";
 })
 export class AccessoriesPageComponent {
     products: ProductItem5Model [] = [];
+    products2: ProductItem5ModelPt2 [] = [];
 
     constructor() {
       for (var product of mock_product_list5) {
         console.log(product);
         this.products.push(product);
+      }
+      for (var product2 of mock_product_list5_2) {
+        console.log(product2);
+        this.products2.push(product2)
       }
     }
 }
---
+++ src/app/storefront/accessories/mock_product_list5-2.ts
@@ -0,0 +1,31 @@
+export const mock_product_list5_2 = [
+    {
+        img : "assets/bags.jpg",
+        description: "Bags"
+    },
+    {
+        img : "assets/hats.jpg",
+        description: "Hats"
+    },
+    {
+        img : "assets/jewelry.jpg",
+        description: "Jewelry"
+    },
+    {
+        img : "assets/hairaccessories.jpg",
+        description: "Hair Accessories"
+    },
+    {
+        img : "assets/cellphoneaccessories.jpg",
+        description: "Cell Phone Accessories"
+    },
+    {
+        img : "assets/socks.jpg",
+        description: "Socks"
+    },
+    {
+        img : "assets/belts.jpg",
+        description: "Belts"
+    }
+
+];
