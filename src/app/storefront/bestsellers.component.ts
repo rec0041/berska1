12e1d54885f7bc0511b432b2765d01687e65bdbb
Rachel <rec0041@mix.wvu.edu>
2022-03-08 13:10:20 -0500

    lab 7

--- src/app/storefront/bestsellers.component.ts
+++ src/app/storefront/bestsellers.component.ts
@@ -1,19 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { mock_product_list } from "../mock_product_list";
 import { ProductItemModel } from "../product-item.model";
+import { ProductsService } from "../products.service";
 
 @Component({
     selector: 'bershka-bestsellers',
     templateUrl: 'bestsellers.component.html',
     styleUrls: ['bestsellers.component.css']
 })
-export class BestSellersComponent {
+export class BestSellersComponent implements OnInit{
     products: ProductItemModel [] = [];
 
-  constructor() {
+  constructor(private productsService:ProductsService) {
     for (var product of mock_product_list) {
       console.log(product);
       this.products.push(product);
     }
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe(data => {
+      console.log("Fetching product data");
+      console.log(data);
+    })
   }
 }
